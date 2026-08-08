6e86221ea723a3d3ccf4110851dd85ca97b9871a
Th0rN13 <th0r@inbox.ru>
2026-08-08 23:46:16 +0500

    feat: add initial frontend

---
+++ apps/frontend/prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+	useTabs: true,
+	singleQuote: false,
+	trailingComma: "all",
+	printWidth: 80,
+	endOfLine: "lf",
+	plugins: ["prettier-plugin-svelte"],
+	overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
+};
+
+export default config;
---
+++ apps/frontend/src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Sapa TV web site</h1>
