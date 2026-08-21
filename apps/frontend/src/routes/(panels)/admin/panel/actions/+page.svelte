67356dcf083088fc488edc99e52255ee1296c4b5
Th0rN13 <th0r@inbox.ru>
2026-08-21 09:28:40 +0500

    feat(frontend): add rules\actions management page

---
+++ apps/frontend/src/routes/(panels)/admin/panel/actions/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import ActionsSection from "#lib/components/admin/actions/ActionsSection.svelte";
+	import RulesSection from "#lib/components/admin/actions/RulesSection.svelte";
+</script>
+
+<svelte:head>
+	<title>Sapa TV | Действия и триггеры</title>
+</svelte:head>
+
+<ActionsSection />
+<RulesSection />
